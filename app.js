80b5217fd817f9f70b65075f584eb7d8b94b7350
Simone Cosma <scosmaa@gmail.com>
2018-05-19 20:01:47 +0200

    Integrazione Express + socketIO

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const Gpio = require('onoff').Gpio;
+const Express = require('express');
+let server = Express();
 
-let led = new Gpio(4, 'out'), //#B
+const Gpio = require('onoff').Gpio;
+const http = require('http').Server(server);
+const io = require('socket.io')(http);
+const config = require('./config.json');
+
+server.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+server.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+let led = new Gpio(config.relays[0].pin_id, 'out'), //#B
   interval;
 
 interval = setInterval(function () { //#C
@@ -8,4 +27,4 @@ interval = setInterval(function () { //#C
   led.write(value, function() { //#E
     console.log("Changed LED state to: " + value);
   });
-}, 2000);
+}, 500);
