5639a27ab68cbdf82c93cf80f4face8975dadbe9
Simone Cosma <scosmaa@gmail.com>
2018-05-20 11:14:11 +0200

    Controllo relee usando raspberry

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const Express = require('express');
 let server = Express();
-
-const Gpio = require('onoff').Gpio;
 const http = require('http').Server(server);
 const io = require('socket.io')(http);
+
+
+const Gpio = require('onoff').Gpio;
 const config = require('./config.json');
 
-server.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
+server.use(Express.static('public'));
 
-server.listen(3000, function () {
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
 
+
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('change_value', function(msg){
+    led.write(msg, function() {
+	io.emit('value_changed', "Changed LED state to: " + msg);
+    });
+    console.log('message: ' + msg);
+  });
 });
+
 
 let led = new Gpio(config.relays[0].pin_id, 'out'), //#B
   interval;
 
-interval = setInterval(function () { //#C
-  var value = (led.readSync() + 1) % 2; //#D
-  led.write(value, function() { //#E
-    console.log("Changed LED state to: " + value);
-  });
-}, 500);
+// interval = setInterval(function () { //#C
+//   var value = (led.readSync() + 1) % 2; //#D
+//   led.write(value, function() { //#E
+//     console.log("Changed LED state to: " + value);
+//   });
+// }, 5000);
+
+led.watch(function (err, value) {
+  io.emit('value_changed', "Changed LED state to: " + value);
+});
