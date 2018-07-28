df3eeaf354df2a2c3243917f99080956dde55c06
Simone Cosma <scosmaa@scosmaa-pc.homenet.telecomitalia.it>
2018-07-28 14:16:36 +0200

    Added vue js scaffolding

--- app.js
+++ app.js
@@ -3,45 +3,30 @@ let server = Express();
 const http = require('http').Server(server);
 const io = require('socket.io')(http);
 
-
 const Gpio = require('onoff').Gpio;
 const config = require('./config.json');
 
 server.use(Express.static('public'));
 
+let relays = [];
 
-http.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+config.relays.map((item) => {
+  let relay = new Gpio(item.pin_id, item.direction);
+  relays[item.code] = relay;
+  relay.write(1); // RESET
 });
 
 
-
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
-
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
+  io.emit('config', config);
   console.log('a user connected');
-  socket.on('change_value', function(msg){
-    led.write(msg, function() {
-	io.emit('value_changed', "Changed LED state to: " + msg);
+  socket.on('change_value', function (msg) {
+    relays[msg.code].write(msg.value, function () {
+      io.emit('value_changed', `Changed LED ${msg.code} state to ${msg.value}`);
     });
-    console.log('message: ' + msg);
   });
 });
 
-
-let led = new Gpio(config.relays[0].pin_id, 'out'), //#B
-  interval;
-
-// interval = setInterval(function () { //#C
-//   var value = (led.readSync() + 1) % 2; //#D
-//   led.write(value, function() { //#E
-//     console.log("Changed LED state to: " + value);
-//   });
-// }, 5000);
-
-led.watch(function (err, value) {
-  io.emit('value_changed', "Changed LED state to: " + value);
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
