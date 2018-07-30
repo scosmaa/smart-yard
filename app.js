3328733e0a211db98e556ca1e420740d09157dd3
scosmaa <scosmaa@gmail.com>
2018-07-30 22:59:10 +0200

    scheduler draft

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Express = require('express');
 let server = Express();
 const http = require('http').Server(server);
 const io = require('socket.io')(http);
+const schedule = require('node-schedule');
 
 const Gpio = require('onoff').Gpio;
 const config = require('./config.json');
@@ -33,6 +34,28 @@ io.on('connection', function (socket) {
   socket.on('get_config', function () {
     io.emit('config', config);
   });
+
+  var rule = new schedule.RecurrenceRule();
+    rule.minute = 9;
+
+    var j = schedule.scheduleJob(rule, function(){
+      console.log('Today is recognized by Rebecca Black!');
+      relays['area1'].write(0, function () {
+        statuses['area1'].value = 1;
+        io.emit('value_changed', statuses['area1']);
+      });
+    });
+
+    var rule2 = new schedule.RecurrenceRule();
+    rule2.minute = 10;
+
+    var k = schedule.scheduleJob(rule2, function(){
+      console.log('Today is recognized by Rebecca Black!');
+      relays['area1'].write(1, function () {
+        statuses['area1'].value = 0;
+        io.emit('value_changed', statuses['area1']);
+      });
+    });
 });
 
 
